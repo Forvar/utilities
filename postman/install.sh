278e787c5bd694599cfefa232806c8ad9a7243f4
dpaissan <denis.paissan@gmail.com>
2018-02-05 17:27:57 +0100

    Added postman

---
+++ postman/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Install
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+sudo tar -xzf postman.tar.gz -C /opt
+rm postman.tar.gz
+sudo ln -s /opt/Postman/Postman /usr/bin/postman
+
+#Shortcut
+cat > ~/.local/share/applications/postman.desktop <<EOL
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=postman
+Icon=/opt/Postman/resources/app/assets/icon.png
+Terminal=false
+Type=Application
+Categories=Development;
+EOL
